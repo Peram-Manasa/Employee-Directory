d59c97673b4384721ed7f9e58c57f9825e0c8af3
Peram-Manasa <manasa.peram@zelarsoft.com>
2023-05-09 10:51:12 +0530

    fc

---
+++ SpfxDirectory-main/src/webparts/zemployeedir/components/PeopleList/PeopleList.module.scss.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+require("./PeopleList.module.css");
+const styles = {
+  persona_card: 'persona_card_9561cbd1',
+  persona: 'persona_9561cbd1',
+  callout: 'callout_9561cbd1',
+  calloutShow: 'calloutShow_9561cbd1',
+  pnPPagination: 'pnPPagination_9561cbd1',
+  container: 'container_9561cbd1',
+  applygrid: 'applygrid_9561cbd1',
+  title: 'title_9561cbd1',
+  subTitle: 'subTitle_9561cbd1',
+  description: 'description_9561cbd1',
+  button: 'button_9561cbd1',
+  label: 'label_9561cbd1'
+};
+
+export default styles;
+/* tslint:enable */
